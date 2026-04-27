6fa5f6760e25e41932056b76e3f4d720127e868a
Amit Oved <amitioved@gmail.com>
2026-04-27 13:25:22 +0300

    fix title mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,9 +44,9 @@ export default function HomePage({ onItemClick }) {
         {/* HEADER */}
         <div className="absolute top-12 w-full text-center z-10">
 
-          <h1 className="text-5xl md:text-6xl font-display tracking-[0.35em] text-white">
-            ORIYA NINA
-          </h1>
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-display tracking-[0.3em] text-white whitespace-nowrap">
+		  ORIYA NINA
+		  </h1>
 
           <p className="text-gold-300 text-xs tracking-[0.6em] mt-4 uppercase">
             LUXURY EVENING DRESSES
