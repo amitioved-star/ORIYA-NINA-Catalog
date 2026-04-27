04bbb58184b5160cd1d5e2b8b6eba1c5af83ee07
Amit Oved <amitioved@gmail.com>
2026-04-27 12:37:11 +0300

    add google analytics1

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+/* 🔥 פונקציית מעקב גלובלית */
+window.trackEvent = (name, params = {}) => {
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag('event', name, params)
+  } else {
+    console.warn('Analytics not loaded yet')
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
